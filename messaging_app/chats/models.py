8964f079fe1c2e5d6618d6188c678bb72ed46e6b
Ashraf Magdy Mostafa <ashrafmagdy926@gmail.com>
2025-07-18 13:51:12 +0300

    added the CustomUser Model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    user_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
+    phone_number = models.CharField(max_length=20, blank=True)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
