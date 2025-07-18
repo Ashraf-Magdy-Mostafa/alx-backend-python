010b84a9fd0ddfa44ed9b1fae0ae691297da86fe
Ashraf Magdy Mostafa <ashrafmagdy926@gmail.com>
2025-07-18 14:38:15 +0300

    added Conversation Class to models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,7 +1,10 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 # Create your models here.
+
+"""" Create a CustomUser Class that build upon existing User"""
 
 
 class CustomUser(AbstractUser):
@@ -15,3 +18,24 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Conversation(models.Model):
+    """
+    Represents a conversation between multiple users.
+    """
+    # A unique identifier for the conversation.
+    conversation_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        help_text="Unique identifier for the conversation"
+    )
+    # Many-to-many relationship with the CustomUser model.
+    # This tracks all participants in the conversation.
+    # We use settings.AUTH_USER_MODEL to refer to the custom user model.
+    participants = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='conversations',
+        help_text="Users participating in this conversation"
+    )
