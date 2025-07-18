e8c8816bb87ddb341c4ce9911c9a2063c2e3c3e7
Ashraf Magdy Mostafa <ashrafmagdy926@gmail.com>
2025-07-18 06:20:46 +0300

    added 1 more test to test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -67,6 +67,15 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_get_json.assert_called_once_with(test_url)
             self.assertEqual(mock_public_repos_url.call_count, 1)
 
+        @parameterized.expand([
+            ({"license": {"key": "my_license"}}, "my_license", True),
+            ({"license": {"key": "other_license"}}, "my_license", False),
+        ])
+        def test_has_license(self, repo, license_key, expected):
+            """Test has_license returns True if license matches, else False."""
+            result = GithubOrgClient.has_license(repo, license_key)
+            self.assertEqual(result, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
