0e90e524b0f5604c900532332304d40ea72387cc
Ashraf Magdy Mostafa <ashrafmagdy926@gmail.com>
2025-07-18 14:25:11 +0300

    updated CustomUser model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -7,6 +7,8 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     user_id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max=255)
+    last_name = models.CharField(max=255)
     phone_number = models.CharField(max_length=20, blank=True)
     created_at = models.DateTimeField(auto_now=True)
 
