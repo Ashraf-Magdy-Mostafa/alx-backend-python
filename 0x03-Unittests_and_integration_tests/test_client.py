ea51bc9bbca01b0e09b3963c164f0d091e1db804
Ashraf Magdy Mostafa <ashrafmagdy926@gmail.com>
2025-07-18 06:02:51 +0300

    fixed errors with test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """Unit tests for the GithubOrgClient class."""
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, Mock, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -34,7 +34,7 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient("google")
 
         with patch.object(
-            GithubOrgClient, "org", new_callable=property
+            GithubOrgClient, "org", new_callable=PropertyMock
         ) as mock_org:
             mock_org.return_value = mocked_org_payload
             result = client._public_repos_url
@@ -54,22 +54,18 @@ class TestGithubOrgClient(unittest.TestCase):
         test_url = "https://api.github.com/orgs/test_org/repos"
 
         with patch.object(
-                GithubOrgClient,
-                "_public_repos_url",
-                new_callable=property) as mock_public_repos_url:
-
+            GithubOrgClient,
+            "_public_repos_url",
+            new_callable=PropertyMock
+        ) as mock_public_repos_url:
             mock_public_repos_url.return_value = test_url
 
             repos = client.public_repos()
             expected_repos = ["repo1", "repo2", "repo3"]
             self.assertEqual(repos, expected_repos)
 
-            # Since mock_public_repos_url is a patch on a property,
-            #  assert_called_once()
-            # does not always work as expected. Instead check call count:
-            self.assertEqual(mock_public_repos_url.call_count, 1)
-
             mock_get_json.assert_called_once_with(test_url)
+            self.assertEqual(mock_public_repos_url.call_count, 1)
 
 
 if __name__ == "__main__":
