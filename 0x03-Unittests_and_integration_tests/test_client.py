aaa9897ff3a9e84d9e0b1b6105cfa734e60cd093
Ashraf Magdy Mostafa <ashrafmagdy926@gmail.com>
2025-07-20 12:52:40 +0300

    adjusted code with pycode

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -34,7 +34,9 @@ class TestGithubOrgClient(unittest.TestCase):
         mocked_org_payload = {"repos_url": expected_url}
         client = GithubOrgClient("google")
 
-        with patch.object(GithubOrgClient, "org", new_callable=PropertyMock) as mock_org:
+        with patch.object(
+            GithubOrgClient, "org", new_callable=PropertyMock
+        ) as mock_org:
             mock_org.return_value = mocked_org_payload
             result = client._public_repos_url
             self.assertEqual(result, expected_url)
@@ -52,7 +54,9 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient("test_org")
         test_url = "https://api.github.com/orgs/test_org/repos"
 
-        with patch.object(GithubOrgClient, "_public_repos_url", new_callable=PropertyMock) as mock_public_repos_url:
+        with patch.object(
+            GithubOrgClient, "_public_repos_url", new_callable=PropertyMock
+        ) as mock_public_repos_url:
             mock_public_repos_url.return_value = test_url
 
             repos = client.public_repos()
@@ -86,7 +90,8 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        """Patch requests.get with side_effect to return mock .json() values."""
+        """Patch requests.get with side_effect
+        to return mock .json() values."""
         cls.get_patcher = patch("requests.get")
         mock_get = cls.get_patcher.start()
 
